ee8d6c7fa4f114f7421650af70719ce4c42a1f57
Brian Chuo <brianchuo56@gmail.com>
2019-06-18 22:58:49 -0400

    Added Region class
    Defined Region class, along with its parameters, xLoc, yLoc, etc.

---
+++ M4/app/src/main/java/com/example/m4/model/Region.java
@@ -0,0 +1,44 @@
+package com.example.m4.model;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a region of planets, defined using an ArrayList of Planet objects.
+ * Constructor requires no parameters.
+ */
+public class Region {
+    ArrayList<Planet> planetList = new ArrayList<>();
+    int numPlanets;
+    int xLoc;
+    int yLoc;
+
+    /**
+     * Default constructor initializing all the variables
+     */
+    public Region () {
+        numPlanets = 0;
+        xLoc = 0;
+        yLoc = 0;
+    }
+
+    /**
+     * Adds a new planet to the ArrayList, also increments the number of planets the region has
+     *
+     * @param planet Planet to be added to the list
+     */
+    public void addPlanet(Planet planet) {
+        planetList.add(planet);
+        numPlanets++;
+    }
+
+    //Getters and setters for the variables
+    public int getNumPlanets() { return numPlanets; }
+
+    public int getxLoc() { return xLoc; }
+
+    public int getyLoc() { return yLoc; }
+
+    public void setxLoc(int xLoc) { this.xLoc = xLoc; }
+
+    public void setyLoc(int yLoc) { this.yLoc = yLoc; }
+}
