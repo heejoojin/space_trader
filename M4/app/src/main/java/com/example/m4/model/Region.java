d4fa7e608b2fa7d703256ff7b1389542d017117c
heejoojin <hjin77@gatech.edu>
2019-06-20 13:15:08 -0400

    Update Region.java

--- M4/app/src/main/java/com/example/m4/model/Region.java
+++ M4/app/src/main/java/com/example/m4/model/Region.java
@@ -6,39 +6,64 @@ import java.util.ArrayList;
  * Represents a region of planets, defined using an ArrayList of Planet objects.
  * Constructor requires no parameters.
  */
-public class Region {
-    ArrayList<Planet> planetList = new ArrayList<>();
-    int numPlanets;
-    int xLoc;
-    int yLoc;
+public enum Region {
 
-    /**
-     * Default constructor initializing all the variables
-     */
-    public Region () {
-        numPlanets = 0;
-        xLoc = 0;
-        yLoc = 0;
+    PREAGRICULTURE("Pre-Agriculture"), AGRICULTURE("Agriculture"),
+    MEDIEVAL("Medieval"), RENAISSANCE("Renaissance"), EARLYINDUSTRIAL("Early Industrial"),
+    INDUSTRIAL("Industrial"), POSTINDUSTRIAL("Post-Industrial"),
+    HITECH(" Hi-Tech");
+
+    private String region;
+
+    Region(String region) {
+        this.region = region;
     }
 
-    /**
-     * Adds a new planet to the ArrayList, also increments the number of planets the region has
-     *
-     * @param planet Planet to be added to the list
-     */
-    public void addPlanet(Planet planet) {
-        planetList.add(planet);
-        numPlanets++;
+    public String getRegion() {
+        return region;
     }
 
-    //Getters and setters for the variables
-    public int getNumPlanets() { return numPlanets; }
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
-    public int getxLoc() { return xLoc; }
+    @Override
+    public String toString() {
+        return region;
+    }
 
-    public int getyLoc() { return yLoc; }
-
-    public void setxLoc(int xLoc) { this.xLoc = xLoc; }
-
-    public void setyLoc(int yLoc) { this.yLoc = yLoc; }
+//    ArrayList<Planet> planetList = new ArrayList<>();
+//    int numPlanets;
+//    int xLoc;
+//    int yLoc;
+//
+//    /**
+//     * Default constructor initializing all the variables
+//     */
+//    public Region () {
+//        numPlanets = 0;
+//        xLoc = 0;
+//        yLoc = 0;
+//    }
+//
+//    /**
+//     * Adds a new planet to the ArrayList, also increments the number of planets the region has
+//     *
+//     * @param planet Planet to be added to the list
+//     */
+//    public void addPlanet(Planet planet) {
+//        planetList.add(planet);
+//        numPlanets++;
+//    }
+//
+//    //Getters and setters for the variables
+//    public int getNumPlanets() { return numPlanets; }
+//
+//    public int getxLoc() { return xLoc; }
+//
+//    public int getyLoc() { return yLoc; }
+//
+//    public void setxLoc(int xLoc) { this.xLoc = xLoc; }
+//
+//    public void setyLoc(int yLoc) { this.yLoc = yLoc; }
 }
