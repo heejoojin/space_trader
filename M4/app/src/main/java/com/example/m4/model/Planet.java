c388e829797265b8f0755095f6de849ae247f158
redchu18 <redchu138@gmail.com>
2019-06-20 20:32:39 -0400

    changed Planet, added empty enum classes

--- M4/app/src/main/java/com/example/m4/model/Planet.java
+++ M4/app/src/main/java/com/example/m4/model/Planet.java
@@ -1,40 +1,54 @@
 package com.example.m4.model;
 
 public class Planet {
-//    private PlanetName planetName;
-//    private Colors color;
-//    private int xLocation;
-//    private int yLocation;
-//
-//    public void setPlanetName(PlanetName planetName) {
-//        this.planetName = planetName;
-//    }
-//
-//    public void setColor(Colors color) {
-//        this.color = color;
-//    }
-//
-//    public void setxLocation(int xLocation) {
-//        this.xLocation = xLocation;
-//    }
-//
-//    public void setyLocation(int yLocation) {
-//        this.yLocation = yLocation;
-//    }
-//
-//    public PlanetName getPlanetName() {
-//        return planetName;
-//    }
-//
-//    public Colors getColor() {
-//        return color;
-//   }
-//
-//    public int getxLocation() {
-//        return xLocation;
-//    }
-//
-//    public int getyLocation() {
-//        return yLocation;
-//    }
+
+    private PlanetName planetName;
+    private Colors color;
+    private int xLocation;
+    private int yLocation;
+    private RegionName regionName;
+
+    //constructors for a planet object
+
+    public Planet(PlanetName planetName, Colors color, int xLocation, int yLocation, RegionName regionName) {
+        this.planetName = planetName;
+        this.color = color;
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+        this.regionName = regionName;
+    }
+
+    //getters and setters for the variable
+
+    public PlanetName getPlanetName() {
+        return planetName;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public int getxLocation() {
+        return xLocation;
+    }
+
+    public int getyLocation() {
+        return yLocation;
+    }
+
+    public void setPlanetName(PlanetName planetName) {
+        this.planetName = planetName;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public void setxLocation(int xLocation) {
+        this.xLocation = xLocation;
+    }
+
+    public void setyLocation(int yLocation) {
+        this.yLocation = yLocation;
+    }
 }
---
+++ M4/app/src/main/java/com/example/m4/model/RegionName.java
@@ -0,0 +1,5 @@
+package com.example.m4.model;
+
+public enum RegionName {
+    //enum stuff here
+}
