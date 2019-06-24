4a59dc3297af6e2c26efda786fc431472694999e
redchu18 <redchu138@gmail.com>
2019-06-23 21:49:44 -0400

    changed numPlanets

--- M4/app/src/main/java/com/example/m4/views/CreatePlayerView.java
+++ M4/app/src/main/java/com/example/m4/views/CreatePlayerView.java
@@ -69,7 +69,7 @@ public class CreatePlayerView extends AppCompatActivity implements OnClickListen
     @Override
     public void onClick (View v) {
         if (v.getId() == R.id.create_universe_button) {
-            Universe universe = new Universe(10, 16);
+            Universe universe = new Universe(10, 30);
             universe.populate();
             String string = universe.toString();
             Log.d("Universe", string);
