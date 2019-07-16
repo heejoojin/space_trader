a6db093ec63eebbdb0c596f2a90e1fd1c699597d
Nina Moorman <ninamoorman@gmail.com>
2019-07-15 21:18:37 -0400

    not quite done junits

--- M4/app/src/main/java/com/example/m4/repository/Repository.java
+++ M4/app/src/main/java/com/example/m4/repository/Repository.java
@@ -29,34 +29,75 @@ public class Repository {
     public static ArrayList<Planet> toTravelPlanets;
 
 
+    /**
+     * set Player Class method
+     * @param p player Class
+     */
     public static void setPlayerClass(Player p) {
         playerClass = p;
     }
 
+    /**
+     * set Universe Class
+     * @param u universe
+     */
     public static void setUniverseClass(Universe u) {
         universeClass = u;
     }
 
+    /**
+     * set Region class
+     * @param r region
+     */
     public static void setRegionClass(Region r) {
         regionClass = r;
     }
 
+    /**
+     * set Planet class
+     * @param pl planet
+     */
     public static void setPlanetClass(Planet pl) {
         planetClass = pl;
     }
 
+    /**
+     * set Items list
+     * @param il ArrayList of Items
+     */
     public static void setItemsList(ArrayList<Item> il) {
         itemsList = il;
     }
 
+    /**
+     * set Mercenaries List
+     * @param ml ArrayList of mercenaries
+     */
     public static void setMercenariesList(ArrayList<Mercenary> ml) { mercenariesList = ml; }
 
+    /**
+     * set Ship List
+     * @param sl arraylist of ships
+     */
     public static void setShipList(ArrayList<Ship> sl) {shipList = sl; }
 
+    /**
+     * set Ship Class
+     * @param s ship
+     */
     public static void setShipClass(Ship s) { shipClass = s; }
 
+    /**
+     * set toTravel region name
+     * @param rn region name
+     */
     public static void setToTravelRegionName(RegionName rn) { toTravelRegionName = rn; }
-    public static void setToTravelPlanets(ArrayList<Planet> pls) { toTravelPlanets = pls; }
 
+
+    /**
+     * set toTravel planets
+     * @param pls arraylist of planets
+     */
+    public static void setToTravelPlanets(ArrayList<Planet> pls) { toTravelPlanets = pls; }
 
 }
