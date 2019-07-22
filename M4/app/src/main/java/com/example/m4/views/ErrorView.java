bb1712c4a6daf5322228e45a83fb03d889a38681
Nina Moorman <ninamoorman@gmail.com>
2019-07-21 20:23:12 -0400

    movie

--- M4/app/src/main/java/com/example/m4/views/ErrorView.java
+++ M4/app/src/main/java/com/example/m4/views/ErrorView.java
@@ -1,6 +1,10 @@
 package com.example.m4.views;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.m4.R;
@@ -20,6 +24,13 @@ public class ErrorView extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error);
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.movie;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController((mediaController));
+        mediaController.setAnchorView(videoView);
     }
 }
 
